e3a3f22a4bd5a17c744dc50f73d750f8fca9d561
Walter Francis <wally@theblackmoor.net>
2016-01-18 11:14:55 -0500

    Fixed typo in readme on step parameter info and created Examples directory in which I've put the default config file as well as an airband example.  As other examples make sense I can add more.

---
+++ Examples/air.pm
@@ -0,0 +1,15 @@
+# Air band
+
+$type = "scan";
+$mode = "am";
+$start = "118e6";
+$end = "137e6";
+$step = "25000";
+$delaylevel = "-40";
+$delaytime = "1";
+$pause = "0.1";
+$wait = "1";
+$levelstop = "-50";
+$coloroutput = "1";
+
+1;
